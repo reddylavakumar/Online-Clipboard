f563a555a4f8b215c087f1f58f80029c87fe34a5
Lava kumar <91463556+reddylavakumar@users.noreply.github.com>
2024-03-21 22:10:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,39 +4,40 @@ const cors = require("cors");
 const DataModel = require('./models/Data');
 
 const app = express();
-// app.use(cors(
-//     {
-//         origin: ["https://online-clipboard-nine.vercel.app"],
-//         methods: ["POST","GET"],
-//         credentials:true
-//     }   
-//     ));
+
+// Configure CORS
 app.use(cors({
-    origin: "*",
-    methods: ["POST", "GET"]
+    origin: "https://online-clipboard-nine.vercel.app",
+    methods: ["POST", "GET"],
+    credentials: true
 }));
-                 
+
+// Body parser middleware
 app.use(express.json());
 
+// MongoDB connection
 mongoose.connect("mongodb+srv://lavakumar9877:Lav%409877@cluster0.qbrgzki.mongodb.net/data?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Test route
 app.get("/", (req, res) => {
   res.send("<h1>Server online </h1>");
 });
 
-
+// Register route
 app.post('/register', (req, res) => {
     DataModel.create(req.body)
         .then(information => res.json(information))
         .catch(err => res.status(500).json({ error: err.message }));
 });
 
+// Retrieve route
 app.get('/retrieve', (req, res) => {
     DataModel.find()
         .then(information => res.json(information))
         .catch(err => res.status(500).json({ error: err.message }));
 });
 
+// Retrieve data by uniqueId route
 app.get('/api/data/:uniqueId', (req, res) => {
     const uniqueId = req.params.uniqueId;
     DataModel.findOne({ uniid: uniqueId })
@@ -49,6 +50,7 @@ app.get('/api/data/:uniqueId', (req, res) => {
         .catch(err => res.status(500).json({ error: err.message }));
 });
 
+// Start the server
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
